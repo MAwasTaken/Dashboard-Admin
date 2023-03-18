c21908255a7a52ab83d5699837fd4605c4f5cc97
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-03-18 22:43:02 +0330

    poject routes created.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import routes from "./routes";
+
+export default function App() {
+	let router = useRoutes(routes);
+
+	return <>{router}</>;
+}
