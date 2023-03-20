5d3122e0c7bea312d0084d426cabd848d78d92da
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-03-20 23:07:19 +0330

    Topbar Created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
+import Topbar from "./components/Topbar/Topbar";
+import Sidebar from "./components/Sidebar/Sidebar";
 import routes from "./routes";
+import './App.css'
 
 export default function App() {
 	let router = useRoutes(routes);
 
-	return <>{router}</>;
+	return (
+		<>
+			<Topbar />
+			<div className='container'>
+				{router}
+				<Sidebar />
+			</div>
+		</>
+	);
 }
---
+++ src/components/Topbar/Topbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import LanguageIcon from "@mui/icons-material/Language";
+import SettingsIcon from "@mui/icons-material/Settings";
+import "./Topbar.css";
+
+export default function Topbar() {
+	return (
+		<div className='topbar'>
+			<div className='topbar-wrapper'>
+				<div className='top-left'>
+					<span className='logo'>MAwasTaken ♥︎</span>
+				</div>
+				<div className='top-right'>
+					<div className='topbar-icon-container'>
+						<NotificationsIcon />
+						<span className='top-icon-badge'>2</span>
+					</div>
+					<div className='topbar-icon-container'>
+						<LanguageIcon />
+						<span className='top-icon-badge'>2</span>
+					</div>
+					<div className='topbar-icon-container'>
+						<SettingsIcon />
+					</div>
+					<img className="top-avatar" src='images/profile.jfif' />
+				</div>
+			</div>
+		</div>
+	);
+}
